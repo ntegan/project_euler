028239c6abd38914a2618f9463ccfeaaef55e966
nick <nicholas.egan@starlab.io>
2020-05-26 20:42:04 -0500

    yeet

--- ac_three/src/main.rs
+++ ac_three/src/main.rs
@@ -1,3 +1,16 @@
+use zzz_print_problem_header::header;
+
 fn main() {
-    println!("Hello, world!");
+    header::problem(3);
+
+    let answer = three::get_largest_prime_factor(15);
+    println!("{}", answer);
+}
+//<p>The prime factors of 13195 are 5, 7, 13 and 29.</p>
+//<p>What is the largest prime factor of the number 600851475143 ?</p>
+
+mod three {
+    pub fn get_largest_prime_factor(num: u64) -> u64 {
+        3
+    }
 }
