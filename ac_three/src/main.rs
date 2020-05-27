f7cb04a9a06c757dc1cf15ff4a070a8e1bbc5ab0
nick <nicholas.egan@starlab.io>
2020-05-27 07:47:34 -0500

    Finish problem 3
    Took 69 minutes to run (see info.html)

--- ac_three/src/main.rs
+++ ac_three/src/main.rs
@@ -3,14 +3,36 @@ use zzz_print_problem_header::header;
 fn main() {
     header::problem(3);
 
-    let answer = three::get_largest_prime_factor(15);
+    let answer = three::get_largest_prime_factor(600851475143);
     println!("{}", answer);
 }
-//<p>The prime factors of 13195 are 5, 7, 13 and 29.</p>
-//<p>What is the largest prime factor of the number 600851475143 ?</p>
 
+// Very inefficient
 mod three {
     pub fn get_largest_prime_factor(num: u64) -> u64 {
-        3
+        // all potential factors: [1,num)
+        //let a: Vec<u64> = (1..num)
+        let a: Option<u64> = (1..num)
+            // get factors of num
+            .filter(|&b: &u64|{ is_factor_of(b, num) }) 
+            // filter out the nonprime(composite) factors
+            .filter(|&b: &u64|{ is_prime(b) })
+            .last();
+            //.collect();
+
+        match a {
+            Some(b) => b,
+            None => 0
+        }
+    }
+    // is a a factor of b?
+    fn is_factor_of(a: u64, b: u64) -> bool { (b % a) == 0 }
+    fn is_prime(n: u64) -> bool {
+        //  definition of a prime number
+        //  only factors 1 and itself
+        let factors: Vec<u64> = (1..n)
+            .filter(|&b: &u64|{ is_factor_of(b, n) }).collect();
+
+        factors.len() <= 1
     }
 }
